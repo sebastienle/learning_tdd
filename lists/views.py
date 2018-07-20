2524fef06509c3c4606024e03f779e959e95761d
Sebastien Lessard <sebastien.lessard@outlook.com>
2018-07-20 12:39:01 -0400

    Remove code for creating new items from home page

--- lists/views.py
+++ lists/views.py
@@ -3,20 +3,16 @@ from django.http import HttpResponse
 
 from lists.models import Item
 
-# Create your views here.
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
-    else:
-        items = Item.objects.all()
-        return render(request, 'home.html', {'items': items})
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
 
 def list_view(request):
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
+
 
 def new_list(request):
     new_item_text = request.POST['item_text']
